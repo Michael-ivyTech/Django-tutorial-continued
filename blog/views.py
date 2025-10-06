789510bfd4d8426baa6e22d029bb9d21956d6277
Michael-ivyTech <mbaker423@ivytech.edu>
2025-10-06 09:40:22 -0400

    Update

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.http import request
 
-
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
