26b1a3e30d2b03b63fe8f47e28fbbdef4d440cc6
Michael-ivyTech <mbaker423@ivytech.edu>
2025-10-07 09:32:10 -0400

    Added post_draft_list.html

--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,17 @@ from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 from django.http import request
 
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -33,7 +37,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
